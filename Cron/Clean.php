4b45d7a11d5326d0a937c30f90887e579e4e295f
Joshua Rice <jrice@lacrossefootwear.com>
2021-12-10 17:38:43 -0500

    Implement record lifetime

--- Cron/Clean.php
+++ Cron/Clean.php
@@ -2,37 +2,45 @@
 
 namespace Lfi\MonologDatabase\Cron;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Lfi\MonologDatabase\Api\Data\RecordInterface;
 use Lfi\MonologDatabase\Model\ResourceModel\Record;
 
 class Clean
 {
-    const JOB_CODE = 'lfi_monolog_database_clean';
-
+    protected $config;
     protected $resource;
 
 
-    public function __construct(Record $resource)
+    public function __construct(ScopeConfigInterface $config, Record $resource)
     {
+        $this->config = $config;
         $this->resource = $resource;
     }
 
-    protected function getThreshold($lifetime)
+    /**
+     * Get the record lifetime threshold based on the number of days.
+     * @param int $days
+     * @return string
+     */
+    protected function getThreshold(int $days): string
     {
         $dt = new \DateTime();
-        $offset = new \DateInterval($lifetime);
+        $offset = new \DateInterval("P{$days}D");
         $dt->sub($offset);
         return $dt->format('Y-m-d H:i:s');
     }
 
+    /**
+     * @throws \Magento\Framework\Exception\LocalizedException from \Magento\Framework\Model\ResourceModel\Db\AbstractDb::getMainTable
+     */
     public function execute(): void
     {
-        $threshold = 0;
-
+        $lifetime = (int) $this->config->getValue('system/monolog_database/lifetime');
         $conn = $this->resource->getConnection();
         $conn->delete(
             $this->resource->getMainTable(),
-            $conn->quoteInto(RecordInterface::DATETIME . ' < ?', $threshold)
+            $conn->quoteInto(RecordInterface::DATETIME . ' < ?', $this->getThreshold($lifetime))
         );
     }
 }
